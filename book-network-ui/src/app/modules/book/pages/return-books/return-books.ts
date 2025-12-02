2be1257383b7e6fd9719541672d9ab1f830c8e66
sohamohsencic23 <Soha.Mohsen@cic.ae>
2025-12-02 10:51:47 +0200

    Merge remote-tracking branch 'origin/main'

---
+++ book-network-ui/src/app/modules/book/pages/return-books/return-books.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-return-books',
+  imports: [],
+  templateUrl: './return-books.html',
+  styleUrl: './return-books.scss',
+})
+export class ReturnBooks {
+
+}
